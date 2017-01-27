e8977e2f43f23313c868209a5a679dc20f6cd781
danielizag <01daniel.izaguirre10@gmail.com>
2017-01-27 01:20:37 -0500

    Update bot4.js

--- Practice/bot4.js
+++ Practice/bot4.js
@@ -13,17 +13,17 @@ stream.on('follow', followed)
 function followed(eventMsg){
 	var name = eventMsg.source.name;
 	var screenName = eventMsg.source.screen_name;
-	tweetIt('@' + screenName + ' thanks for the follow!')
+	tweetSend('@' + screenName + ' thanks for the follow!')
 }
 
-function tweetIt(txt){
+function tweetSend(text){
 
 	console.log('You got a follow!');
 	
 	var r = Math.floor(Math.random()*100);
 
 	var tweet = {
-	status: txt
+	status: text
 	}
 
 	T.post('statuses/update', tweet, tweeted);
